3ddc03f24d1544f53022ce24f1fc55280e10ca90
Jakub Caban <kuba@whyblack.pl>
2017-12-18 12:51:19 +0100

    Add Vendor, move /img -> /src

--- src/plugins/wpdevart.php
+++ src/plugins/wpdevart.php
@@ -2,27 +2,23 @@
 
 class LockMe_wpdevart{
   static private $options;
-  static private $resdata;
 
   static public function Init(){
-    global $wpdb;
     self::$options = get_option("lockme_wpdevart");
 
     if(self::$options['use']){
-      if($_GET['page'] == "wpdevart-reservations" && is_admin() && $_POST['task']){
-        if($_POST['id']){
-          self::$resdata = $wpdb->get_row($wpdb->prepare('SELECT * FROM ' . $wpdb->prefix . 'wpdevart_reservations WHERE `id`=%d', $_POST['id']), ARRAY_A);
-        }
-      }
-      register_shutdown_function(array('LockMe_wpdevart', 'ShutDown'));
+//       add_action('wpdevart_new_appointment_created', array('LockMe_wpdevart', 'AddEditReservation'), 5);
+//       add_action('transition_post_status', array('LockMe_wpdevart', 'AddEditReservation'), 10, 3 );
+//       add_action('before_delete_post', array('LockMe_wpdevart', 'Delete'));
+//       add_action('edit_post', array('LockMe_wpdevart', 'AddEditReservation'));
 
       add_action('init', function(){
-        if($_GET['wpdevart_export']){
-          LockMe_wpdevart::ExportToLockMe();
-          $_SESSION['wpdevart_export'] = 1;
-          wp_redirect("?page=lockme_integration&tab=wpdevart_plugin");
-          exit;
-        }
+//         if($_GET['wpdevart_export']){
+//           LockMe_wpdevart::ExportToLockMe();
+//           $_SESSION['wpdevart_export'] = 1;
+//           wp_redirect("?page=lockme_integration&tab=wpdevart_plugin");
+//           exit;
+//         }
       });
     }
   }
@@ -39,9 +35,9 @@ class LockMe_wpdevart{
 
     add_settings_section(
           'lockme_wpdevart_section',
-          "Ustawienia wtyczki Booking Calendar Pro WpDevArt",
+          "Ustawienia wtyczki Booked",
           function(){
-            echo '<p>Ustawienia integracji z wtyczką Booking Calendar Pro WpDevArt</p>';
+            echo '<p>Ustawienia integracji z wtyczką Booked</p>';
           },
           'lockme-wpdevart');
 
@@ -82,16 +78,16 @@ class LockMe_wpdevart{
           array()
         );
       }
-      add_settings_field(
-        "export_wpdevart",
-        "Wyślij dane do LockMe",
-        function(){
-          echo '<a href="?page=lockme_integration&tab=wpdevart_plugin&wpdevart_export=1">Kliknij tutaj</a> aby wysłać wszystkie rezerwacje do kalendarza LockMe. Ta operacja powinna być wymagana tylko raz, przy początkowej integracji.';
-        },
-        'lockme-wpdevart',
-        'lockme_wpdevart_section',
-        array()
-      );
+//       add_settings_field(
+//         "export_wpdevart",
+//         "Wyślij dane do LockMe",
+//         function(){
+//           echo '<a href="?page=lockme_integration&tab=wpdevart_plugin&wpdevart_export=1">Kliknij tutaj</a> aby wysłać wszystkie rezerwacje do kalendarza LockMe. Ta operacja powinna być wymagana tylko raz, przy początkowej integracji.';
+//         },
+//         'lockme-wpdevart',
+//         'lockme_wpdevart_section',
+//         array()
+//       );
     }
   }
 
@@ -101,6 +97,10 @@ class LockMe_wpdevart{
       echo '<p>Nie posiadasz wymaganej wtyczki.</p>';
       return;
     }
+//     $appt_id = 1907;
+//     $timeslot = get_post_meta($appt_id);
+//
+//     var_dump($timeslot);
 
     if($_SESSION['wpdevart_export']){
       echo '<div class="updated">';
@@ -113,48 +113,71 @@ class LockMe_wpdevart{
   }
 
   static private function AppData($res){
+    $cal = wp_get_object_terms($res->ID, 'wpdevart_custom_calendars');
+    $timeslot = explode('-', get_post_meta($res->ID, '_appointment_timeslot',true));
+    $time = str_split($timeslot[0], 2);
+
+    if($res->post_author){
+      $user_info = get_userdata($res->post_author);
+      $name = wpdevart_get_name($res->post_author);
+			$email = $user_info->user_email;
+			$phone = get_user_meta($res->post_author, 'wpdevart_phone', true);
+    }
+    $name = get_post_meta($res->ID, '_appointment_guest_name',true) ?: $name;
+    $email = get_post_meta($res->ID, "_appointment_guest_email", true) ?: $email;
 
     return array(
-      'roomid'=>self::$options['calendar_'.$res['calendar_id']],
-      'date'=>date('Y-m-d', strtotime($res['single_day'])),
-      'hour'=>date("H:i:s", strtotime($res['start_hour'])),
+      'roomid'=>self::$options['calendar_'.($cal[0] ? $cal[0]->term_id : 'default')],
+      'date'=>date('Y-m-d', get_post_meta($res->ID, '_appointment_timestamp',true)),
+      'hour'=>date("H:i:s", strtotime("{$time[0]}:{$time[1]}:00")),
+      'name'=>$name,
       'pricer'=>"API",
-      'email'=>$res['email'],
-      'status'=>$res["status"] == "approved" ? 1 : 0,
-      'extid'=>$res['id'],
-      'price'=>$res["price"]
+      'email'=>$email,
+      'phone'=>$phone,
+      'status'=>in_array($res->post_status, array('publish','future')) ? 1 : 0,
+      'extid'=>$res->ID
     );
   }
 
-  static public function AddEditReservation($res){
+  static public function AddEditReservation($id){
     global $lockme;
-    if(!is_array($res)){
+    if(!is_numeric($id)){
       return;
     }
     if(defined("LOCKME_MESSAGING")){
       return;
     }
 
+    $type = get_post_type($id);
+
+    if($type && (get_post_type($id) != 'wpdevart_appointments')){
+      return;
+    }
+
+    $post = get_post($id);
+
+    if(!$post || get_post_status($id) == 'trash'){
+      return self::Delete($id);
+    }
+
     $api = $lockme->GetApi();
-    $id = $res['id'];
-    $resdata = self::AppData($res);
 
     try{
-      $lockme_data = $api->Reservation($resdata["roomid"], "ext/{$id}");
+      $lockme_data = $api->Reservation("ext/{$id}");
     }catch(Exception $e){
     }
 
     try{
       if(!$lockme_data){ //Add new
-        $id = $api->AddReservation($resdata);
+        $id = $api->AddReservation(self::AppData($post));
       }else{ //Update
-        $api->EditReservation($resdata["roomid"], "ext/{$id}", $resdata);
+        $api->EditReservation("ext/{$id}", self::AppData($post));
       }
     }catch(Exception $e){
     }
   }
 
-  static public function Delete($res){
+  static public function Delete($id){
     global $lockme;
 
     if(defined("LOCKME_MESSAGING")){
@@ -162,49 +185,55 @@ class LockMe_wpdevart{
     }
 
     $api = $lockme->GetApi();
-    $id = $res['id'];
-    $resdata = self::AppData($res);
 
     try{
-      $api->DeleteReservation($resdata["roomid"], "ext/{$id}");
+      $api->DeleteReservation("ext/{$id}");
     }catch(Exception $e){
     }
   }
 
-  static public function ShutDown(){
-    global $wpdb;
-    if($_GET['page'] == "wpdevart-reservations" && is_admin() && $_POST['task']){
-      switch($_POST['task']){
-        case "approve":
-          self::AddEditReservation(self::$resdata);
-          break;
-        case "canceled":
-        case "delete":
-          self::Delete(self::$resdata);
-          break;
-      }
-    }
-    if (defined('DOING_AJAX') && DOING_AJAX) {
-      switch($_POST['action']){
-        // case 'wpdevart_form_ajax':
-        //   $data = json_decode(stripcslashes($_POST['wpdevart_data']),true);
-        //   $res = $wpdb->get_row($wpdb->prepare('SELECT * FROM ' . $wpdb->prefix . 'wpdevart_reservations WHERE single_day="%s" and start_hour="%s" and calendar_id=%d', $data['wpdevart_single_day1'], $data['wpdevart_form_hour1'], $_POST['wpdevart_id']),ARRAY_A);
-        //   self::AddEditReservation($res);
-        //   break;
-      }
-    }
-  }
-
   static private function GetCalendar($roomid){
-    global $wpdb;
-    $query = "SELECT * FROM " . $wpdb->prefix . "wpdevart_calendars ";
-    $calendars = $wpdb->get_results($query);
+
+    $calendars = get_terms('wpdevart_custom_calendars','orderby=slug&hide_empty=0');
     foreach($calendars as $calendar){
-      if(self::$options["calendar_".$calendar->id] == $roomid){
-        return $calendar->id;
+      if(self::$options["calendar_".$calendar->term_id] == $roomid){
+        return $calendar->term_id;
       }
     }
     return null;
+  }
+
+  static private function GetSlot($calendar_id, $date, $hour){
+    $wpdevart_defaults = get_option('wpdevart_defaults_'.$calendar_id);
+    if (!$wpdevart_defaults){
+      $wpdevart_defaults = get_option('wpdevart_defaults');
+    }
+
+		$day_name = date('D',$date);
+    $formatted_date = date_i18n('Ymd',$date);
+    if(function_exists("wpdevart_apply_custom_timeslots_details_filter")){
+      $wpdevart_defaults = wpdevart_apply_custom_timeslots_details_filter($wpdevart_defaults,$calendar_id);
+    }else if(function_exists("wpdevart_apply_custom_timeslots_filter")){
+      $wpdevart_defaults = wpdevart_apply_custom_timeslots_filter($wpdevart_defaults,$calendar_id);
+    }
+
+    if (isset($wpdevart_defaults[$formatted_date]) && !empty($wpdevart_defaults[$formatted_date])){
+			$todays_defaults = (is_array($wpdevart_defaults[$formatted_date]) ? $wpdevart_defaults[$formatted_date] : json_decode($wpdevart_defaults[$formatted_date],true));
+		}elseif (isset($wpdevart_defaults[$formatted_date]) && empty($wpdevart_defaults[$formatted_date])){
+			$todays_defaults = false;
+		}elseif (isset($wpdevart_defaults[$day_name]) && !empty($wpdevart_defaults[$day_name])){
+			$todays_defaults = $wpdevart_defaults[$day_name];
+		}else{
+			$todays_defaults = false;
+		}
+
+		$hour = date("Hi", strtotime($hour));
+		foreach($todays_defaults as $h=>$cnt){
+      if(preg_match("/^{$hour}/", $h)){
+        return $h;
+      }
+		}
+		return null;
   }
 
   static public function GetMessage($message){
@@ -216,13 +245,15 @@ class LockMe_wpdevart{
     $data = $message["data"];
     $roomid = $message["roomid"];
     $lockme_id = $message["reservationid"];
-    $date = $data['date'];
-    $hour = date("H:i", strtotime($data['hour']));
+    $date = strtotime($data['date']);
 
     $calendar_id = self::GetCalendar($roomid);
-    if(!$calendar_id){
-      throw new Exception("No calendar");
+    $hour = self::GetSlot($calendar_id, $date, $data['hour']);
+    if(!$hour){
+      throw new Exception("No time slot");
     }
+		$time_format = get_option('time_format');
+		$date_format = get_option('date_format');
 
     $cf_meta_value = '';
     foreach(array("Żródło"=>"LockMe","Telefon"=>$data['phone'],"Ilość osób"=>$data['people'],"Cena"=>$data['price'],"Status"=>$data['status']?"Opłacone":"Rezerwacja (max. 20 minut)") as $label=>$value){
@@ -231,136 +262,81 @@ class LockMe_wpdevart{
 
     switch($message["action"]){
       case "add":
-        $result = $wpdb->insert($wpdb->prefix.'wpdevart_reservations', array(
-          'calendar_id' => $calendar_id,
-          'single_day' => $date,
-          'start_hour' => $hour,
-          'count_item' => 1,
-          'price' => $data['price'],
-          'total_price' => $data['price'],
-          'form' => array(),
-          'email' => $data['email'],
-          'status' => 'approved',
-          'date_created' => date('Y-m-d H:i',time()),
-          'is_new' => 0
-        ));
-        if(!$result){
-          throw new Exception("Error saving to database - ".$wpdb->last_error);
+				$post = apply_filters('wpdevart_new_appointment_args', array(
+					'post_title' => date_i18n($date_format,$date).' @ '.date_i18n($time_format,$date).' (User: Guest)',
+					'post_content' => '',
+					'post_status' => $data['status'] ? 'publish' : 'draft',
+					'post_date' => date('Y',strtotime($date)).'-'.date('m',strtotime($date)).'-01 00:00:00',
+					'post_type' => 'wpdevart_appointments'
+				));
+        $row_id = wp_insert_post( $post );
+        if(!$row_id || is_wp_error($row_id)){
+          if(is_wp_error($row_id)){
+            throw new Exception($row_id->get_error_message());
+          }else{
+            throw new Exception("Error saving to database: ".$wpdb->last_error);
+          }
         }
+				update_post_meta($row_id, '_appointment_guest_name', $data['name'].' '.$data['surname']);
+				update_post_meta($row_id, '_appointment_guest_email', $data['email']);
+				update_post_meta($row_id, '_appointment_timestamp', $date);
+				update_post_meta($row_id, '_appointment_timeslot', $hour);
+				update_post_meta($row_id, '_appointment_source', "LockMe");
+				update_post_meta($row_id, '_cf_meta_value', $cf_meta_value);
 
-        $id = $wpdb->insert_id;
-
-        $res = $wpdb->get_row($wpdb->prepare('SELECT * FROM ' . $wpdb->prefix . 'wpdevart_reservations WHERE `id`=%d', $id), ARRAY_A);
-
-        $main = new wpdevart_Main;
-    		$theme_model = new wpdevart_bc_ModelThemes();
-    		$calendar_model = new wpdevart_bc_ModelCalendars();
-    		$form_model = new wpdevart_bc_ModelForms();
-    		$extra_model = new wpdevart_bc_ModelExtras();
-        $ids = $calendar_model->get_ids($calendar_id);
-        $theme_option = $theme_model->get_setting_rows($ids["theme_id"]);
-    		$calendar_data = $calendar_model->get_db_days_data($calendar_id);
-    		$calendar_title = $calendar_model->get_calendar_rows($calendar_id);
-    		$calendar_title = $calendar_title["title"];
-    		$extra_field = $extra_model->get_extra_rows($ids["extra_id"]);
-    		$form_option = $form_model->get_form_rows($ids["form_id"]);
-        if(isset($theme_option)){
-    			$theme_option = json_decode($theme_option->value, true);
-    		} else {
-    			$theme_option = array();
-    		}
-        $wpdevart_booking = new wpdevart_bc_BookingCalendar($date, $res, $calendar_id, $theme_option, $calendar_data, $form_option, $extra_field, array(), false, array(), $calendar_title);
-
-        $reflector = new ReflectionObject($wpdevart_booking);
-        $method = $reflector->getMethod('change_date_avail_count');
-        $method->setAccessible(true);
-        $method->invoke($wpdevart_booking, $id, true, "insert", array());
+        if (isset($calendar_id) && $calendar_id){
+          wp_set_object_terms($row_id, $calendar_id, 'wpdevart_custom_calendars');
+        }
 
         try{
           $api = $lockme->GetApi();
-          $api->EditReservation($roomid, $lockme_id, array("extid"=>$id));
-          return true;
+          $api->EditReservation($lockme_id, array("extid"=>$row_id));
         }catch(Exception $e){
         }
         break;
       case "edit":
         if($data['extid']){
           $row_id = $data["extid"];
-
-          $old_reserv = $wpdb->get_row($wpdb->prepare('SELECT * FROM ' . $wpdb->prefix . 'wpdevart_reservations WHERE `id`=%d', $row_id), ARRAY_A);
-
-          $result = $wpdb->update($wpdb->prefix.'wpdevart_reservations', array(
-            'calendar_id' => $calendar_id,
-            'single_day' => $date,
-            'start_hour' => $hour,
-            'count_item' => 1,
-            'price' => $data['price'],
-            'total_price' => $data['price'],
-            'form' => array(),
-            'email' => $data['email'],
-            'status' => 'approved',
-            'date_created' => date('Y-m-d H:i',time()),
-            'is_new' => 0
-          ), array('id' => $row_id));
-
-          $main = new wpdevart_Main;
-      		$theme_model = new wpdevart_bc_ModelThemes();
-      		$calendar_model = new wpdevart_bc_ModelCalendars();
-      		$form_model = new wpdevart_bc_ModelForms();
-      		$extra_model = new wpdevart_bc_ModelExtras();
-          $ids = $calendar_model->get_ids($calendar_id);
-          $theme_option = $theme_model->get_setting_rows($ids["theme_id"]);
-      		$calendar_data = $calendar_model->get_db_days_data($calendar_id);
-      		$calendar_title = $calendar_model->get_calendar_rows($calendar_id);
-      		$calendar_title = $calendar_title["title"];
-      		$extra_field = $extra_model->get_extra_rows($ids["extra_id"]);
-      		$form_option = $form_model->get_form_rows($ids["form_id"]);
-          if(isset($theme_option)){
-      			$theme_option = json_decode($theme_option->value, true);
-      		} else {
-      			$theme_option = array();
-      		}
-          $wpdevart_booking = new wpdevart_bc_BookingCalendar($date, $old_reserv, $calendar_id, $theme_option, $calendar_data, $form_option, $extra_field, array(), false, array(), $calendar_title);
-
-          $reflector = new ReflectionObject($wpdevart_booking);
-          $method = $reflector->getMethod('change_date_avail_count');
-          $method->setAccessible(true);
-          $method->invoke($wpdevart_booking, $row_id, true, "update", $old_reserv);
-
-          return true;
+          $post = apply_filters('wpdevart_new_appointment_args', array(
+            'ID' => $row_id,
+            'post_title' => date_i18n($date_format,$date).' @ '.date_i18n($time_format,$date).' (User: Guest)',
+            'post_content' => '',
+            'post_status' => $data['status'] ? 'publish' : 'draft',
+            'post_date' => date('Y',strtotime($date)).'-'.date('m',strtotime($date)).'-01 00:00:00',
+            'post_type' => 'wpdevart_appointments'
+          ));
+          wp_update_post($post, $wp_error);
+          update_post_meta($row_id, '_appointment_guest_name', $data['name'].' '.$data['surname']);
+          update_post_meta($row_id, '_appointment_guest_email', $data['email']);
+          update_post_meta($row_id, '_appointment_timestamp', $date);
+          update_post_meta($row_id, '_appointment_timeslot', $hour);
+          if(get_post_meta($row_id, '_appointment_source', true) == 'LockMe'){
+            update_post_meta($row_id, '_cf_meta_value', $cf_meta_value);
+          }
         }
         break;
       case 'delete':
         if($data["extid"]){
-          $row_id = $data["extid"];
-
-          $old_reserv = $wpdb->get_row($wpdb->prepare('SELECT * FROM ' . $wpdb->prefix . 'wpdevart_reservations WHERE `id`=%d', $row_id), ARRAY_A);
-
-          $delete_res = $wpdb->query($wpdb->prepare('DELETE FROM '.$wpdb->prefix.'wpdevart_reservations WHERE id="%d"', $row_id));
-
-          require_once(WPDEVART_PLUGIN_DIR . 'admin/controllers/Reservations.php');
-
-          $wpdevart_booking = new wpdevart_bc_ControllerReservations();
-          $reflector = new ReflectionObject($wpdevart_booking);
-          $method = $reflector->getMethod('change_date_avail_count');
-          $method->setAccessible(true);
-          $method->invoke($wpdevart_booking, $row_id, false, $old_reserv);
-
-          return true;
+          wp_delete_post($data["extid"]);
         }
         break;
     }
   }
 
   static public function ExportToLockMe(){
-    global $wpdb;
-    set_time_limit(0);
-
-    $sql = "SELECT * FROM {$wpdb->prefix}wpdevart_reservations WHERE `single_day` >= curdate() ORDER BY ID";
-    $rows = $wpdb->get_results($sql, ARRAY_A);
-
-    foreach($rows as $row){
-      self::AddEditReservation($row);
+    $args = array(
+      'post_type' => 'wpdevart_appointments',
+      'orderby' => 'meta_value',
+      'meta_key' => '_appointment_timestamp',
+      'posts_per_page' => -1
+    );
+    $loop = new WP_Query( $args );
+    while ( $loop->have_posts() ){
+      $loop->the_post();
+      $post = $loop->post;
+      if(get_post_meta($post->ID, "_appointment_timestamp", true) >= strtotime("today")){
+        self::AddEditReservation($post->ID);
+      }
     }
   }
 }
