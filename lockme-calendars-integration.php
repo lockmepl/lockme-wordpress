21187d8ad1f9ca434c612b5f7542801580fb7182
Jakub Caban <kuba@whyblack.pl>
2017-12-18 12:56:14 +0100

    Prepare for 1.0.3

--- lockme-calendars-integration.php
+++ lockme-calendars-integration.php
@@ -4,7 +4,7 @@ use Lockme\SDK\Lockme;
 Plugin Name: Lockme calendars integration
 Plugin URI:  https://github.com/Lustmored/lockme
 Description: This plugin integrates popular booking systems with Lockme OAuth2 API.
-Version:     1.0.1
+Version:     1.0.3
 Author:      Jakub Caban
 Author URI:  https://lockme.pl
 License:     GPLv2 or later
