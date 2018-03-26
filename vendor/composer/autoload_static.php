29cd69a46df9cd9a684051954e36fe47d47c6236
lustmored <lustmored@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-03-26 11:32:44 +0000

    Composer update
    git-svn-id: https://plugins.svn.wordpress.org/lockme-calendars-integration/trunk@1846928 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -9,8 +9,8 @@ class ComposerStaticInitaa28e8270f1181f78a62f7ff5c0ad807
     public static $files = array (
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
-        '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
+        '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
     );
 
     public static $prefixLengthsPsr4 = array (
