66486cb48a8e9a05555061effc21b5c3102450b9
lustmored <lustmored@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2020-07-09 09:54:23 +0000

    Prepare for bump
    git-svn-id: https://plugins.svn.wordpress.org/lockme-calendars-integration/trunk@2337922 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lockme-calendars-integration.php
+++ lockme-calendars-integration.php
@@ -6,7 +6,7 @@ use LockmeIntegration\Plugin;
 Plugin Name: Lockme calendars integration
 Plugin URI:  https://github.com/Lustmored/lockme
 Description: This plugin integrates popular booking systems with Lockme OAuth2 API.
-Version:     1.3.4
+Version:     1.3.5
 Author:      Jakub Caban
 Author URI:  https://lockme.pl
 License:     GPLv2 or later
