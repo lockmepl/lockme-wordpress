8b324615c081913b4058921220f846dd9b784664
lustmored <lustmored@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2023-08-24 08:09:13 +0000

    Update deps and metadata
    git-svn-id: https://plugins.svn.wordpress.org/lockme-calendars-integration/trunk@2957711 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- build/vendor/composer/autoload_real.php
+++ build/vendor/composer/autoload_real.php
@@ -35,15 +35,15 @@ class ComposerAutoloaderInitd4cbf4ed8610848fdd2afdd92decc223
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitd4cbf4ed8610848fdd2afdd92decc223::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link:
 Tags: lustmored
 Requires PHP: 7.2
 Requires at least: 4.8
-Tested up to: 6.0.0
+Tested up to: 6.1.1
 Stable tag: 2.3.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
